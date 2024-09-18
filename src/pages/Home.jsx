1f1f554d134eda713ad8f0e6e4ec44b0cc03d74e
gastonMacias <gastonezequielmacias@gmail.com>
2024-09-18 01:54:12 -0300

    pagina principal del portfolio

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+// import Footer from "../components/footer/Footer";
+// import Navbar from "../components/navbar/Navbar";
+import SeccionPresentacion from "../components/seccionPresentacion/SeccionPresentacion";
+
+import "./home.css";
+
+const Home = () => {
+    return <>
+        {/* <Navbar /> */}
+        <SeccionPresentacion />
+        {/* <Footer /> */}
+    </>
+}
+
+
+export default Home;
