5de04d02c773befc69241ba6b3110157e477faf6
gastonMacias <gastonezequielmacias@gmail.com>
2025-07-29 17:59:17 -0300

    se especifico desarrollo

--- src/components/seccionPresentacion/SeccionPresentacion.jsx
+++ src/components/seccionPresentacion/SeccionPresentacion.jsx
@@ -4,7 +4,7 @@ import "./seccionPresentacion.css";
 const SeccionPresentacion = () => {
     return  <section className="presentacion">
     <h2 className="nombre">Hola, soy <span className="nombre-destaque">Gastón</span></h2>
-    <h3 className="rol">Desarrollador Frontend & Tester QA</h3>
+    <h3 className="rol">Desarrollador Frontend REACT Jr.  & Tester QA</h3>
     <p className="descripcion">
       Además, soy <span className="destaque">Técnico Electrónico</span> con pasión por la tecnología y la innovación.
     </p>
